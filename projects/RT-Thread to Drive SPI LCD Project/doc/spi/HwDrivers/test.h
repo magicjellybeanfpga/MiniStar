755112178f83e3f2f6981d46a68c85832bde20d6
magicjellybeanfpga <81582905+magicjellybeanfpga@users.noreply.github.com>
2021-09-15 13:47:38 -0400

    Add files via upload

---
+++ projects/RT-Thread to Drive SPI LCD Project/doc/spi/HwDrivers/test.h
@@ -0,0 +1,19 @@
+#ifndef __TEST_H__
+#define __TEST_H__
+
+//#include "main.h"
+#include "gw1ns4c.h"
+
+void DrawTestPage(uint8_t *str);
+void Test_Color(void);
+void Test_FillRec(void);
+void Test_Circle(void);
+void Test_Triangle(void);
+void English_Font_test(void);
+void Chinese_Font_test(void);
+void Pic_test(void);
+void Load_Drow_Dialog(void);
+void Touch_Test(void);
+void main_test(void);
+void Rotate_Test(void);
+#endif
